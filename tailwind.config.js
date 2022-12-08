52b2c961cab3f049ad6772230675bc228d60ab51
2019000010081 <motalibpathan11@gmail.com>
2022-12-08 13:42:33 +0600

    some changes for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
-    fontFamily: ["Inter", "sans-serif"],
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
   },
   plugins: [],
 };
